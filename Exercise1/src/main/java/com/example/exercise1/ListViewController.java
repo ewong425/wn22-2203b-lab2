4457e88f278d847b84e31b8c7586c58e1e126978
Ethan Wong <ewong425@uwo.ca>
2023-01-26 12:31:48 -0500

    finished ex1

--- Exercise1/src/main/java/com/example/exercise1/ListViewController.java
+++ Exercise1/src/main/java/com/example/exercise1/ListViewController.java
@@ -1,9 +1,12 @@
 package com.example.exercise1;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 
 public class ListViewController {
     @FXML
@@ -11,15 +14,23 @@ public class ListViewController {
     @FXML
     private ListView<String> lvItem;
     @FXML
-    private Button listViewBtn;
-    @FXML
     private Label selectedItem;
+    @FXML
+    private Button listViewBtn;
+
     @FXML
     public void initialize() {
         lvItem.getItems().addAll("Poodle", "Great Dane", "Labrador", "Terrier");
+
+        lvItem.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        ObservableList<String> selections = lvItem.getSelectionModel().getSelectedItems();
+        String[] itemArray = selections.toArray(new String[0]);
+        ObservableList<String> strList = FXCollections.observableArrayList(itemArray);
+        // Populate the ListView control:
+        lvItem.getItems().addAll(strList);
     }
     @FXML
     void displaySelectedItem() {
-        selectedItem.setText(lvItem.getSelectionModel().getSelectedItem());
+        selectedItem.setText(" " + lvItem.getSelectionModel().getSelectedItems());
     }
 }
